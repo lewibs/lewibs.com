be4bb7d427baa237f361cf130439f6f3460ac030
lewibs <benjaminsl2000@gmail.com>
2022-03-18 00:44:14 -0400

    started work on version3

---
+++ lewibs3.0/src/App.js
@@ -0,0 +1,14 @@
+import Me from './Me';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="background">
+        <Me />
+      </div>
+    </div>
+  );
+}
+
+export default App;
