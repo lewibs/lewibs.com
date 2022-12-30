90c3f9b53ec6d1acac76d08a00af1e62ff91d41f
lewibs <benjaminsl2000@gmail.com>
2022-12-30 10:03:35 -0500

    updated meta

--- src/js/Skills.js
+++ src/js/Skills.js
@@ -1,6 +1,19 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import '../css/skills.css';
+import { meta } from '../meta';
 
 function Skills() {
+    const [web, setWeb] = useState("");
+    const [software, setSoftware] = useState("");
+    const [languages, setLanguages] = useState("");
+
+    useEffect(()=>{
+        axios.get(meta.skills.web).then(({data})=>setWeb(data));
+        axios.get(meta.skills.software).then(({data})=>setSoftware(data));
+        axios.get(meta.skills.languages).then(({data})=>setLanguages(data));
+    },[])
+
     return (
         <section id='skills' className='background4 color1 section'>
             <h2>Skills and Expertise</h2>
@@ -9,19 +22,19 @@ function Skills() {
                 <span className='skill'>
                     <i className="fa fa-code"></i>
                     <h4>Web/App Development</h4>
-                    <div>React, axios, potree, three.js, IFC.js, potree, nativescript jquery, styled-components, html/css</div>
+                    <div>{web}</div>
                 </span>
 
                 <span className='skill'>
                     <i className="fa fa-tv color1"></i>
                     <h4>Software Development</h4>
-                    <div>AWS, cPanel, Spring, eclipse, Mongoose, Nestjs, linux, mongoDB, gin, node, OOP, functional, SQL, Spring, Hibernate</div>
+                    <div>{software}</div>
                 </span>
 
                 <span className='skill'>
                     <i className="fa fa-pencil"></i>
                     <h4>Languages</h4>
-                    <div>javascript, typescript, golang, python, java, c, assembly, shell, MATLAB, haskell, php</div>
+                    <div>{languages}</div>
                 </span>
             </div>
         </section>  
--- src/meta.js
+++ src/meta.js
@@ -1,3 +1,8 @@
 export const meta = {
-    about: "https://raw.githubusercontent.com/lewibs/lewibs.com/3.0/meta/aboutMe.txt"
+    about: "https://raw.githubusercontent.com/lewibs/lewibs.com/3.0/meta/aboutMe.txt",
+    skills: {
+        web: "https://raw.githubusercontent.com/lewibs/lewibs.com/3.0/meta/skills/web.txt",
+        software: "https://raw.githubusercontent.com/lewibs/lewibs.com/3.0/meta/skills/software.txt",
+        lannguages: "https://raw.githubusercontent.com/lewibs/lewibs.com/3.0/meta/skills/language.txt"
+    }
 }
