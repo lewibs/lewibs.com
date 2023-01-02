9eb6ff57beccfd8fa276602e6379429a0f8a48f2
lewibs <benjaminsl2000@gmail.com>
2023-01-02 16:23:02 -0500

    changed link to blue cause its standard for clickable text

--- src/content/Projects.js
+++ src/content/Projects.js
@@ -64,7 +64,7 @@ const About = styled.div`
 
 const More = styled.div`
     cursor: pointer;
-    color: ${colors.tertiaryDark};
+    color: ${colors.secondary};
 `;
 
 const ProjectTitle = styled.div`
